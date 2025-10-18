17b293c84496eae5fa67b037c8b2121fd71f36b5
GeorginaRetaLimas <2430043@upv.edu.mx>
2025-10-17 19:30:24 -0600

    Creacion de constructor y destructor

--- Simulador2D.h
+++ Simulador2D.h
@@ -23,3 +23,41 @@ public:
     void simularPaso(); // Implementación del algoritmo
     // ... otros métodos de acceso y visualización
 };
+
+template <typename T>
+Simulador2D<T>::Simulador2D(int f, int c)
+    : _filas(f), _columnas(c), _numFuentes(0), _capacidadFuentes(4){
+    
+    // Creamos el array de punteros que tendra las filas
+    _grid = new T*[_filas];
+
+    // Crear las columnas
+    for(int i = 0; i < _filas; i++){
+        _grid[i] = new T[_columnas];
+    
+        // Inicializamos
+        for(int j = 0; j < _columnas; j++) {
+            _grid[i][j] = T(0); // Inicializamos en 0
+        }
+    }
+
+    // Creamos el vector de la capacidad de las fuentes
+    _fuentes = new T[_capacidadFuentes];
+
+    _constantes[0] = 0.25f; // Coeficiente de difisión
+    _constantes[1] = 1.0f;  // Delta X
+    _constantes[2] = 1.0f;  // Delta Y
+}
+
+// Destructor :3
+template <typename T>
+Simulador2D<T>::~Simulador2D(){
+    // Liberamos cada fila
+    for(int i = 0; i < filas; i++){
+        delete[] _grid[i];
+    }
+    
+    // Limpiamos los arrays
+    delete[] _grid;
+    delete[] _fuentes;
+}
